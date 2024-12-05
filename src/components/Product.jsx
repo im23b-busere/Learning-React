556a25b1d4c62d3f5db50eaf2643ae1540c1daea
im23b-busere <busere@bzz.ch>
2024-12-05 13:42:46 +0100

    Transformed CSS --> tailwind CSS

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,8 +3,8 @@ export default function Product(props) {
         <div>
             <img src={props.link} alt="Product"/>
             <h2>{props.name}</h2>
-            <p className="color-prop">{props.color}</p>
-            <p className="price-prop">{props.price}</p>
+            <p className="color-prop text-gray-500">{props.color}</p>
+            <p className="price-prop font-bold">{props.price}</p>
         </div>
     );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,28 +3,28 @@ import Product from './Product.jsx';
 export default function ProductList() {
     return (
         <>
-            <h1>Product List</h1>
-            <ul className="product-list">
-                <li>
+            <h1 className="mt-4 mb-4">Product List</h1>
+            <ul className="product-list flex justify-between list-none text-center">
+                <li className="border border-gray-500 p-2 shadow">
                     <Product
                         link="https://picsum.photos/300"
                         name="Product 1"
                         color="Natural"
                         price="75$"/>
                 </li>
-                <li>
+                <li className="border border-gray-500 p-2 shadow">
                     <Product link="https://picsum.photos/300"
                              name="Product 2"
                              color="Black"
                              price="70$"/>
                 </li>
-                <li>
+                <li className="border border-gray-500 p-2 shadow">
                     <Product link="https://picsum.photos/300"
                              name="Product 3"
                              color="Light Brown"
                              price="27$"/>
                 </li>
-                <li>
+                <li className="border border-gray-500 p-2 shadow">
                     <Product link="https://picsum.photos/300"
                             name="Product 4"
                             color="Walnut"
@@ -34,4 +34,3 @@ export default function ProductList() {
         </>
     );
 }
-
