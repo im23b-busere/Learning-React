1e8c802a9a9148697dc4dcd82cdeb298b7f4cdc1
im23b-busere <busere@bzz.ch>
2025-01-14 14:38:22 +0100

    Update singleProduct placeholder

--- src/components/singleProduct.jsx
+++ src/components/singleProduct.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import BaseLayout from "../layout/Base";
+
 
 export default function SingleProduct() {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/products/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data));
+    }, [id]);
+
+    if (!product) {
+        return <p>Loading...</p>;
+    }
+
     return (
-        <>
-            <p>This is a placeholder for the single product view.</p>
-        </>
+        <div>
+            <BaseLayout></BaseLayout>
+
+            <h1>{product.title}</h1>
+            <img src={product.thumbnail} alt={product.title} />
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+
+        </div>
     );
 }
